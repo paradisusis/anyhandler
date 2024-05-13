f2668f267f4dac696afe692aa8d302e893d19d30
Victor <paradisusvic@gmail.com>
2024-05-13 15:29:21 -0400

    Add OnOptionsToolStripMenuItemDropDownItemClicked()

--- AnyHandler/MainForm.cs
+++ AnyHandler/MainForm.cs
@@ -296,7 +296,14 @@
         /// <param name="e">Event arguments.</param>
         private void OnOptionsToolStripMenuItemDropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
         {
+            // Set tool strip menu item
+            ToolStripMenuItem toolStripMenuItem = (ToolStripMenuItem)e.ClickedItem;
 
+            // Toggle checked
+            toolStripMenuItem.Checked = !toolStripMenuItem.Checked;
+
+            // Set topmost by check box
+            this.TopMost = this.alwaysOnTopToolStripMenuItem.Checked;
         }
 
         /// <summary>
