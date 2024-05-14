8e18e402a827e8c4c17cc315612640b3b36cdf9f
Victor <paradisusvic@gmail.com>
2024-05-13 21:00:55 -0400

    Add OnOriginalThreadDonationCodercomToolStripMenuItemClick()

--- AnyHandler/MainForm.cs
+++ AnyHandler/MainForm.cs
@@ -359,7 +359,8 @@
         /// <param name="e">Event arguments.</param>
         private void OnOriginalThreadDonationCodercomToolStripMenuItemClick(object sender, EventArgs e)
         {
-
+            // Open original thread
+            Process.Start("https://www.donationcoder.com/forum/index.php?topic=54149.0");
         }
 
         /// <summary>
